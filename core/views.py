d79603d0ab17b41d52abc44437361c7e4a0aa3d3
Mahabubhasan01 <mahabubhasaan@outlook.com>
2023-07-16 02:24:53 +0600

    notifications impliment

--- core/views.py
+++ core/views.py
@@ -188,4 +188,26 @@ def Home(request):
          },
 
     ]
-    return render(request, 'core/home.html', {'teachers': teachers, 'students': students})
+    """ https://i.ibb.co/0YKGRWF/download.jpg
+    https://i.ibb.co/cbbRCtX/images-1.jpg
+    https://i.ibb.co/M6cqx1c/images-2.jpg
+    https://i.ibb.co/QpN2RFG/images-3.jpg
+    https://i.ibb.co/c106r85/images-4.jpg
+    https://i.ibb.co/zs4ZXpR/images-5.jpg
+    https://i.ibb.co/fXXtjyM/images-6.jpg
+    https://i.ibb.co/7zJPhHk/images-7.jpg
+    https://i.ibb.co/m5wXjnf/images-8.jpg
+    https://i.ibb.co/zry9pW7/images-9.jpg
+    https://i.ibb.co/DWHwkZx/images-10.jpg
+    https://i.ibb.co/9gDpgF3/images-11.jpg
+    https://i.ibb.co/H7S2H8r/images-12.jpg
+    https://i.ibb.co/mRKMjvv/images-13.jpg
+    https://i.ibb.co/MhhTc8z/images-14.jpg
+    https://i.ibb.co/yk5p7Fd/images-15.jpg
+    https://i.ibb.co/PGgkCMy/images.jpghttps://i.ibb.co/LrwqkN3/download-1.jpg """
+    carousel = [{
+        'name': '',
+        "img": "https: // i.ibb.co/0YKGRWF/download.jpg",
+        "info": ""
+    }]
+    return render(request, 'core/home.html', {'teachers': teachers, 'students': students,"carousel":carousel})
