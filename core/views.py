dcc55457a234dd5e9b91785dda21d498fbfff824
Mahabubhasan01 <mahabubhasaan@outlook.com>
2023-07-20 00:53:28 +0600

    user profile and also some ui design subjects sections

--- core/views.py
+++ core/views.py
@@ -211,6 +211,123 @@ teachers = [
      "moto": "Innovative, charismatic, influential, visionary, approachable, inspiring, dynamic, motivational, knowledgeable, transformational."
      },
 ]
+students = [
+    {"name": "John Smith",
+     "gpa": 5.00,
+     "motto": "Achieve greatness through education, perseverance, and a relentless spirit.",
+     "session": 2020,
+     "img": "https://i.ibb.co/0YKGRWF/download.jpg"
+
+     },
+    {"name": "John Smith",
+     "gpa": 5.00,
+     "motto": "Achieve greatness through education, perseverance, and a relentless spirit.",
+     "session": 2020,
+     "img": "https://i.ibb.co/cbbRCtX/images-1.jpg"
+
+     },
+    {"name": "John Smith",
+     "gpa": 5.00,
+     "motto": "Achieve greatness through education, perseverance, and a relentless spirit.",
+     "session": 2020,
+     "img": "https://i.ibb.co/M6cqx1c/images-2.jpg"
+
+     },
+    {"name": "John Smith",
+     "gpa": 5.00,
+     "motto": "Achieve greatness through education, perseverance, and a relentless spirit.",
+     "session": 2020,
+     "img": "https://i.ibb.co/QpN2RFG/images-3.jpg"
+
+     },
+    {"name": "John Smith",
+     "gpa": 5.00,
+     "motto": "Achieve greatness through education, perseverance, and a relentless spirit.",
+     "session": 2020,
+     "img": "https://i.ibb.co/c106r85/images-4.jpg"
+
+     },
+    {"name": "John Smith",
+     "gpa": 5.00,
+     "motto": "Achieve greatness through education, perseverance, and a relentless spirit.",
+     "session": 2020,
+     "img": "https://i.ibb.co/zs4ZXpR/images-5.jpg"
+
+     },
+    {"name": "John Smith",
+     "gpa": 5.00,
+     "motto": "Achieve greatness through education, perseverance, and a relentless spirit.",
+     "img": "https://i.ibb.co/mRKMjvv/images-13.jpg"
+
+     },
+    {"name": "John Smith",
+     "gpa": 5.00,
+     "motto": "Achieve greatness through education, perseverance, and a relentless spirit.",
+     "session": 2020,
+     "img": "https://i.ibb.co/fXXtjyM/images-6.jpg"
+
+     },
+    {"name": "John Smith",
+     "gpa": 5.00,
+     "motto": "Achieve greatness through education, perseverance, and a relentless spirit.",
+     "session": 2020,
+     "img": "https://i.ibb.co/7zJPhHk/images-7.jpg"
+
+     },
+    {"name": "John Smith",
+     "gpa": 5.00,
+     "motto": "Achieve greatness through education, perseverance, and a relentless spirit.",
+     "session": 2020,
+     "img": "https://i.ibb.co/m5wXjnf/images-8.jpg"
+
+     },
+    {"name": "John Smith",
+     "gpa": 5.00,
+     "motto": "Achieve greatness through education, perseverance, and a relentless spirit.",
+     "session": 2020,
+     "img": "https://i.ibb.co/zry9pW7/images-9.jpg"
+
+     },
+    {"name": "John Smith",
+     "gpa": 5.00,
+     "motto": "Achieve greatness through education, perseverance, and a relentless spirit.",
+     "session": 2020,
+     "img": "https://i.ibb.co/DWHwkZx/images-10.jpg"
+
+     },
+    {"name": "John Smith",
+     "gpa": 5.00,
+     "motto": "Achieve greatness through education, perseverance, and a relentless spirit.",
+     "session": 2020,
+     "img": "https://i.ibb.co/9gDpgF3/images-11.jpg"
+
+     },
+    {"name": "John Smith",
+     "gpa": 5.00,
+     "motto": "Achieve greatness through education, perseverance, and a relentless spirit.",
+     "session": 2020,
+     "img": "https://i.ibb.co/MhhTc8z/images-14.jpg"
+
+
+     },
+    {"name": "John Smith",
+     "gpa": 5.00,
+     "motto": "Achieve greatness through education, perseverance, and a relentless spirit.",
+     "session": 2020,
+     "img": "https://i.ibb.co/H7S2H8r/images-12.jpg"
+
+
+     },
+    {"name": "John Smith",
+     "gpa": 5.00,
+     "motto": "Achieve greatness through education, perseverance, and a relentless spirit.",
+     "session": 2020,
+     "img": "https://i.ibb.co/yk5p7Fd/images-15.jpg"
+
+
+     },
+
+]
 
 
 def Home(request):
@@ -397,3 +514,22 @@ def Dashbord_Home(request):
 def Subjects_Manager(request):
 
     return render(request, 'core/dashboard/subjectsmanager.html', {'teachers': teachers, 'navs': dashnav})
+
+
+def Users_Manager(request):
+
+    return render(request, 'core/dashboard/users.html', {'teachers': teachers, 'students': students, 'navs': dashnav})
+
+
+def Staffs_Manager(request):
+
+    return render(request, 'core/dashboard/staffs.html', {'teachers': teachers, 'students': students, 'navs': dashnav})
+
+
+def Fees_Manager(request):
+
+    return render(request, 'core/dashboard/fee.html', {'teachers': teachers, 'students': students, 'navs': dashnav})
+
+
+def User_Profile(request):
+    return render(request, 'core/dashboard/userprofile.html', {'navs': dashnav})
